a732655b90530cebdc9fd7a7f0aebf589adc59b5
Joaquin Rapela <joacorapela@gmail.com>
2021-01-26 15:59:45 +0000

    Added divide and logAbs

--- calculator.py
+++ calculator.py
@@ -1,9 +1,20 @@
 """
 Calculator library containing basic math operations.
 """
+import math
 
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def divide(numerator, denominator):
+    return numerator / denominator
+
+def logAbs(x): # pragma: no cover
+    if x<0:
+        x *= -1
+    return math.log(x)
