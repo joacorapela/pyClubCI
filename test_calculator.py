9f1a0b3e370c1631537048f61b590742c7b44bbe
Joaquin Rapela <joacorapela@gmail.com>
2021-01-26 16:01:12 +0000

    Added

---
+++ test_calculator.py
@@ -0,0 +1,27 @@
+"""
+Unit tests for the calculator library
+"""
+
+import math
+import calculator
+
+
+# class RandomName:
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
+
+    def test_division(self):
+        assert 2 == calculator.divide(4, 2)
+
+    '''
+    def test_lobAbs(self):
+        assert 3 == calculator.logAbs(math.exp(3))
+    '''
+
+    def test_lobAbsNegative(self):
+        assert 3 == calculator.logAbs(-math.exp(3))
